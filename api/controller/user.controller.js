c3de89001c3d8372bd7b5129094ee03e0dff23ed
Aditya <aditya892003.gmail.com>
2023-11-25 01:10:53 +0530

    create listing api created

--- api/controller/user.controller.js
+++ api/controller/user.controller.js
@@ -50,19 +50,6 @@ export const deleteUser = async (req, res, next) => {
   }
 };
 
-export const getUserListings = async (req, res, next) => {
-  if (req.user.id === req.params.id) {
-    try {
-      const listings = await Listing.find({ userRef: req.params.id });
-      res.status(200).json(listings);
-    } catch (error) {
-      next(error);
-    }
-  } else {
-    return next(errorHandler(401, "You can only view your own listings!"));
-  }
-};
-
 export const getUser = async (req, res, next) => {
   try {
     const user = await User.findById(req.params.id);
@@ -74,5 +61,18 @@ export const getUser = async (req, res, next) => {
     res.status(200).json(rest);
   } catch (error) {
     next(error);
+  }
+};
+
+export const getUserListings = async (req, res, next) => {
+  if (req.user.id === req.params.id) {
+    try {
+      const listings = await Listing.find({ userRef: req.params.id });
+      res.status(200).json(listings);
+    } catch (error) {
+      next(error);
+    }
+  } else {
+    return next(errorHandler(401, "You can only view your own listings!"));
   }
 };
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -3,7 +3,9 @@ import {
   testRoute,
   updateUser,
   deleteUser,
+  getUserListings,
 } from "../controller/user.controller.js";
+
 import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
@@ -12,5 +14,6 @@ const router = express.Router();
 router.get("/test", testRoute);
 router.post("/update/:id", verifyToken, updateUser);
 router.delete("/delete/:id", verifyToken, deleteUser);
+router.get("/listing/:id", verifyToken, getUserListings);
 
 export default router;
