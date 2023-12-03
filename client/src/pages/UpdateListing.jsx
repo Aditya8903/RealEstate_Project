1a89cf88708cee5398f7e004698aa192f606e662
Aditya <aditya892003.gmail.com>
2023-12-03 15:45:45 +0530

    update listing function completed

--- client/src/pages/UpdateListing.jsx
+++ client/src/pages/UpdateListing.jsx
@@ -44,7 +44,7 @@ useEffect(()=>{
         const listingId = params.listingId;
         console.log(listingId);
         const res = await fetch(`/api/listing/get/${listingId}`);
-
+        const data = await res.json();
         if(data.success===false){
             console.log(data.message);
             return
@@ -168,17 +168,18 @@ useEffect(()=>{
         return setError('Discount price must be lower than regular price');
       setLoading(true);
       setError(false);
-      const res = await fetch('/api/listing/create', {
+      const res = await fetch(`/api/listing/update/${params.listingId}`, {
         method: 'POST',
         headers: {
-          'Content-Type': 'application/json',
+            'Content-Type': 'application/json',
         },
         body: JSON.stringify({
-          ...formData,
-          userRef: currentUser._id,
+            ...formData,
+            userRef: currentUser._id,
         }),
-      });
-      const data = await res.json();
+    });
+    const data = await res.json();
+    
       setLoading(false);
       if (data.success === false) {
         setError(data.message);
